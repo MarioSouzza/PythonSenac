cb61d7cfe53e31e778778e161d16012533fe7991
MarioSouzza <mariosouzza49@gmail.com>
2024-10-10 19:31:44 -0300

    Update Aula01.py

--- Modulo 02/Aula01.py
+++ Modulo 02/Aula01.py
@@ -4,10 +4,13 @@ import sqlite3
 conn = sqlite3.connect('mydatabase.db')
 
 #  Criar uma tabela
-conn.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+conn.execute('''CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)''') 
 
 # Salvar as alterações
 conn.commit()
 
 # Fechar a conexão com o banco de dados
 conn.close()
+
+
+print('-'*50)
