2e4c36f35b37a07bd910462dbad3973623c35d46
MarioSouzza <mariosouzza49@gmail.com>
2024-10-10 19:16:47 -0300

    Modulo 01
    Todas as aulas do modulo 01

--- Modulo 01/Aula13.py
+++ Modulo 01/Aula13.py
@@ -11,6 +11,7 @@
 
 # No terminal: pip install requests openpyxl
 
+import openpyxl
 import requests
 
 # URL da API
@@ -21,18 +22,17 @@ response = requests.get(url)
 
 # Verificando o status da requisição
 
-if response.status_code ==200:
-    users = response.json() # Convertendo a resposta em formato JSON
+if response.status_code == 200:
+    users = response.json()  # Convertendo a resposta em formato JSON
     print('-'*50)
     for user in users:
         # if user['name'] =="Leanne Graham":
-            print(f"Nome: {user['name']}, E-mail: {user['email']}, Telefone: {user['phone']}")        
+        print(
+            f"Nome: {user['name']}, E-mail: {user['email']}, Telefone: {user['phone']}")
 else:
     print(f"Falha ao obter dados. Status code: {response.status_code}")
 print('-'*50)
 
-import requests
-import openpyxl
 
 # URL da API
 url = "https://jsonplaceholder.typicode.com/users"
@@ -44,17 +44,18 @@ if response.status_code == 200:
     # Criando um novo arquivo em Excel
     workbook = openpyxl.Workbook()
     sheet = workbook.active
-    
+
     # Adicionando os cabeçalhos
     sheet.append(["ID", "Nome", "Email", "Empresa"])
 
     # Adicionando dados da API ao Excel
     for user in users:
-        sheet.append([user['id'], user['name'], user['email'], user['company']['name']])
-    
+        sheet.append([user['id'], user['name'],
+                     user['email'], user['company']['name']])
+
     # Salvando o arquivo
     workbook.save('usarios_api.xlsx')
-    print ("Dados salvos no arquivo 'usuarios_api.xlsx'.")
+    print("Dados salvos no arquivo 'usuarios_api.xlsx'.")
 else:
     print(f"Falha ao obter dados. Status code: {response.status_code}")
 print('-'*50)
@@ -65,22 +66,23 @@ url = "https://loteriascaixa-api.herokuapp.com/api/megasena"
 # Fazendo a requisição GET para a API
 response = requests.get(url)
 if response.status_code == 200:
-    loterias  = response.json()
+    loterias = response.json()
     print(loterias)
     # Criando um novo arquivo em Excel
     workbook = openpyxl.Workbook()
     sheet = workbook.active
-    
+
     # Adicionando os cabeçalhos
-    sheet.append(["Concurso", "Valor do Prêmio", "Estados Premiados"])
+    sheet.append(["Concurso", "local", "Estados Premiados"])
 
     # Adicionando dados da API ao Excel
     for loteria in loterias:
-        sheet.append(f"{loteria['concurso']}, {loteria['valorPremio']}, {loteria['estadosPremiados']}")
-    
+        sheet.append(
+            f"{loteria['concurso']}, {loteria['local']}, {loteria['estadosPremiados']}")
+
     # Salvando o arquivo
     workbook.save('loteria_api.xlsx')
-    print ("Dados salvos no arquivo 'usuarios_api.xlsx'.")
+    print("Dados salvos no arquivo 'usuarios_api.xlsx'.")
 else:
     print(f"Falha ao obter dados. Status code: {response.status_code}")
 print('-'*50)
